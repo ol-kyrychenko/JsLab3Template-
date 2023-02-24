c5f3816620e485007373d12e145a72451e5b1fe5
ol-kyrychenko <123631399+ol-kyrychenko@users.noreply.github.com>
2023-02-24 11:45:27 +0200

    Add new files

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const host = 'localhost';
+const port = 8000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('Hello from Server!!!!');
+});
+
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
